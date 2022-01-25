9a4e8147ad77ef7d6e62adf28ef6f8dd3af5a6f4
Nick Cho <nicholas.cho@hotmail.ca>
2022-01-25 15:02:19 -0500

    saving progress

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "90b711dc39bf1934"; }
+/******/ 		__webpack_require__.h = function() { return "681d7def80698eb0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
