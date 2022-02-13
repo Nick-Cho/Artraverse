34135f41a778d807879c32c7d76e628b7bbc1b1c
Nick Cho <nicholas.cho@hotmail.ca>
2022-02-13 11:21:17 -0500

    Adding meta tags to SEO site

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,14 +2,31 @@ import {UserContext} from '../context/index';
 import {useContext, useEffect} from "react";
 import axios from 'axios';
 import Post from "../components/cards/Post";
-
+import Head from "next/head";
 const Home = ({posts}) =>{
   const [state,setState] = useContext(UserContext); //gives access to global state
-  useEffect(()=>{
-    console.log(posts);
-  })
+  
+  const head = () => {
+    <Head>
+      <title>A social network for artists </title>
+      <meta 
+      name="description" 
+      content="A social network for artists"
+      />
+      <meta 
+      property="og:description"
+      content="A social network for artists"
+      />
+      <meta property="og:type" content="website"/>
+      <meta property="og:site_name" content=""/>
+      <meta property="og:url" content=""/>
+      <meta property="og:image:secure_url" content=""/>
+    </Head>
+  }
+  
   return(
     <div className = 'container'>
+      
       <div className = "row">
         <div className = 'col'>
           <h1 className = 'display-1 text-center py-5'>Home page</h1>
