8f7d35c7b63e4cacbbec41054c6212113e5c6f30
Nick Cho <65980644+Nick-Cho@users.noreply.github.com>
2022-02-20 09:59:47 -0500

    Creating endpoint to get the logged in user's posts

--- server/routes/post.js
+++ server/routes/post.js
@@ -20,18 +20,23 @@ import {
   removeComment,
   totalPosts,
   posts,
-  getPost
+  getPost,
+  profileFeed
 } from '../controllers/post.js'
 
 router.post('/create-post',requireSignIn, createPost);
+
+//function to attach image to post
 router.post('/upload-image',requireSignIn, formidable({maxFileSize: 5*1024*1024}), uploadImage);
+
 //posts uploaded by the user
 router.get('/user-posts', requireSignIn, postsByUser);
-router.get('/user-post/:_id', requireSignIn, userPost)
+router.get('/user-post/:_id', requireSignIn, userPost); //updates comments and likes to update postin webpage
 router.put('/update-post/:_id', requireSignIn, canEditPost, updatePost)
 router.delete('/delete-post/:_id', requireSignIn, canEditPost, deletePost)
 
 router.get('/news-feed/:page', requireSignIn, newsFeed);
+router.get('/profile-feed/:page', requireSignIn, profileFeed);
 
 router.put('/liked-post', requireSignIn, likePost);
 router.put('/unliked-post', requireSignIn, unlikePost)
@@ -41,7 +46,7 @@ router.put('/remove-comment', requireSignIn, removeComment);
 
 router.get('/total-posts', totalPosts);
 
-//route to get posts in the home page
+//route to get all available posts in the home page
 router.get('/posts', posts);
 
 router.get('/post/:_id', getPost)
